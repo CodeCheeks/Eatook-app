01ddaa503edd9177766598ea50ae347be152fc9c
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-02-22 10:52:41 +0100

    User sign up done with bcryp and saving user data in mongo

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,5 +16,33 @@ module.exports.signup = (req,res,next) => {
 }
 
 module.exports.doSignup = (req,res,next) => {
-    console.log('The form data: ', req.body);
-}
+    //console.log('The form data: ', req.body)      //La información del formulario se manda con el post y se almacena en req.body
+    function renderWithErrors(errors) {
+      res.status(400).render('authentication/signup_form', {
+        errors: errors,
+      })
+    }
+  
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (user) {
+          renderWithErrors({
+            email: 'Ya existe un usuario con este email'
+          })
+        } 
+        else {
+          User.create(req.body)
+            .then(() => {
+              res.redirect('/')
+            })
+            .catch(e => {
+              if (e instanceof mongoose.Error.ValidationError) {
+                renderWithErrors(e.errors)
+              } else {
+                next(e)
+              }
+            })
+        }
+      })
+      .catch(e => next(e))
+  }
--- models/User.model.js
+++ models/User.model.js
@@ -1,45 +1,58 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const { Schema, model } = require('mongoose');
+
+const bcrypt = require('bcrypt')
+const SALT_ROUNDS = 10
+
 const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-const getAuthToken = require('registry-auth-token')
-const getRegistryUrl = require('registry-auth-token/registry-url')
-const SALT_ROUNDS = 10;
 
-const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: 'Email is a required',
-            unique: true,
-            lowercase: true,
-            match: [EMAIL_PATTERN, 'Invalid email. It should content @ and .'],
-            trim: true
-        },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, 'Username is required.'],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      lowercase: true,
+      trim: true,
+      match: [EMAIL_PATTERN, 'Email inválido'],
+      unique: true,
+    },
 
-        username: {
-            type: String,
-            required: 'Username is required',
-            unique: true
-        },
+    password: {
+      type: String,
+      match: [PASSWORD_PATTERN, 'Tu contraseña debe conteneral menos 1 número, 1 mayúscula, 1 minúscula y 8 caracteres'],
+      required: [true, 'Password is required.'],
+    },
 
-        password: {
-            type: String,
-            required: 'Password is required',
-            match: [
-                PASSWORD_PATTERN, 'Your password must have at least one upperCase, one lowerCase,one number and 8 characters'
-            ]
-        },
-
-        role: {
-            type: String,
-            enum: ['admin','user','owner'],
-            default: 'user'
-        }
-
-
+    role: {
+        type: String,
+        enum: ['admin','user','owner'],
+        default: 'user'
     }
-)
+  }
+);
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+userSchema.methods.checkPassword = function (passwordToCheck) {
+  return bcrypt.compare(passwordToCheck, this.password)
+}
+
+userSchema.pre('save', function(next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    bcrypt.hash(user.password, SALT_ROUNDS)
+      .then(hash => {
+        this.password = hash
+        next()
+      })
+  } else {
+    next()
+  }
+})
+ 
+module.exports = model('User', userSchema);
