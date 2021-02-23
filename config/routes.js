db555ce977bc5206be0aa5f520402869d88a90de
Alicia Parra <alicia@redparra.com>
2021-02-23 19:53:48 +0100

    Protect routes

--- config/routes.js
+++ config/routes.js
@@ -2,26 +2,26 @@ const router = require("express").Router()
 const passport = require('passport')
 const miscController = require("../controllers/misc.controller")
 const userController = require("../controllers/user.controller")
-
+const secure = require("../middlewares/secure.middleware");
 // home
 router.get("/", miscController.home)
 
 //sign up
-router.get("/signup", userController.signup)
-router.post("/signup", userController.doSignup)
+router.get("/signup",secure.isNotAuthenticated, userController.signup)
+router.post("/signup",secure.isNotAuthenticated, userController.doSignup)
 
 //login
-router.get("/login", userController.login)
-router.post("/login", userController.doLogin)
+router.get("/login",secure.isNotAuthenticated, userController.login)
+router.post("/login",secure.isNotAuthenticated, userController.doLogin)
 
 //logout
-router.post("/logout", userController.logout)
+router.post("/logout",secure.isAuthenticated, userController.logout)
 
 // profile
-router.get("/profile", userController.profile)
+router.get("/profile", secure.isAuthenticated, userController.profile)
 
 //Activate account
-router.get('/activate/:token', userController.activate)
+router.get('/activate/:token',secure.isNotAuthenticated, userController.activate)
 
 
 
