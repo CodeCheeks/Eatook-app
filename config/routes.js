c84b877448f9b1c713819246ac2102167ab80714
Alicia Parra <alicia@redparra.com>
2021-02-24 19:51:46 +0100

    Fix merge issues

--- config/routes.js
+++ config/routes.js
@@ -3,13 +3,18 @@ const passport = require('passport')
 const miscController = require("../controllers/misc.controller")
 const userController = require("../controllers/user.controller")
 const secure = require("../middlewares/secure.middleware");
+<<<<<<< HEAD
 const upload = require('./storage.config')
+=======
+
+>>>>>>> main
 // home
 router.get("/", miscController.home)
 
 //sign up
-router.get("/signup",secure.isNotAuthenticated, userController.signup)
-router.post("/signup",secure.isNotAuthenticated, userController.doSignup)
+router.get("/signup", secure.isNotAuthenticated, userController.signup)
+router.post("/signup", secure.isNotAuthenticated, userController.doSignup)
+router.get("/signup/verify-account", secure.isNotAuthenticated, userController.verify)
 
 //login
 router.get("/login",secure.isNotAuthenticated, userController.login)
@@ -18,8 +23,21 @@ router.post("/login",secure.isNotAuthenticated, userController.doLogin)
 //logout
 router.post("/logout",secure.isAuthenticated, userController.logout)
 
-// profile
+// USER PROFILE
+//profile main page
 router.get("/profile", secure.isAuthenticated, userController.profile)
+
+//personal information
+router.get("/userinformation", secure.isAuthenticated, userController.userInformation)
+
+//user bookings
+router.get("/bookings", secure.isAuthenticated, userController.userBookings)
+
+//user favourites
+router.get("/userfavourites", secure.isAuthenticated, userController.userFavourites)
+
+//user reviews
+router.get("/userreviews", secure.isAuthenticated, userController.userReviews)
 
 //Activate account
 router.get('/activate/:token',secure.isNotAuthenticated, userController.activate)
