5205ba6234277e13ec13412bb93d04552afb9984
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-03-03 19:56:52 +0100

    fix crud create

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -5,16 +5,27 @@ hbs.registerPartials(path.join(__dirname, "../views/partials"));
 
 
 
-// Comprobar si es user
+
 hbs.registerHelper('isOwner', function (role, options) {
     return role === 'owner' ? options.fn() : options.inverse()
 })
 
-
-// Comprobar si es user
 hbs.registerHelper('checkDay', function (data, day, options) {
+    if(data === undefined){
+        return options.inverse()
+    }
     return data.includes(day) ? options.fn() : options.inverse()
 })
+
+
+// Comprobar si es user
+hbs.registerHelper('checkHour', function (data, hour, options) {
+    if(data === undefined){
+        return options.inverse()
+    }
+    return data.includes(hour) ? options.fn() : options.inverse()
+})
+
 
 
 
